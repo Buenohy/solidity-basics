e01e714870e0615356d38ed36f6fbabd4610937d
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-13 12:31:54 -0300

    feat: variable with sender address and value

--- contracts/GlobalVariables.sol
+++ contracts/GlobalVariables.sol
@@ -3,5 +3,6 @@
 pragma solidity ^0.8.7;
 
 contract GlobalVariables {
-    
+    address public sender = msg.sender; // endereço público de quem está enviando a transação
+    uint public value = msg.value;
 }
