e99ab18a0f837bce8b4f1191a17f5768303fe7f9
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-13 12:07:31 -0300

    feat: add min and max Int

--- contracts/Variables.sol
+++ contracts/Variables.sol
@@ -11,9 +11,11 @@ pragma solidity 0.8.7;
 
 contract Variables {
     bool public boolean = true; // valor padrão false
-    uint8 unInteiro8 = 255; // valor padrão 0 | 2 ** 8 -1
-    uint256 unInteiro256 = 12312312312; // valor padrão 0 | 2 ** 256 -1
-    uint unInteiroAlias = 12312;
+    uint8 public unInteiro8 = 255; // valor padrão 0 | 2 ** 8 -1
+    uint256 public unInteiro256 = 12312312312; // valor padrão 0 | 2 ** 256 -1
+    uint public unInteiroAlias = 12312;
 
-    int interoComNegativo = -1;
+    int public interoComNegativo = 200;
+    int8 public minInt = type(int8).min;
+    int8 public maxInt = type(int8).max;
 }
