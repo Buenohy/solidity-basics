2ed5738a243fc4e8294581f9dc0ec8fa74cc2499
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-13 12:52:09 -0300

    feat: function soma and sub, with external

---
+++ contracts/Function.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract HelperFunctions {
+    function soma(uint256 x, uint256 y) public returns(uint256) {
+        sub(2, 1);
+        return x + y;
+    }
+    function sub(uint256 x, uint256 y) external returns(uint256) {
+        soma(2,3)
+        return x + y;
+    }
+}
