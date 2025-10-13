e983e6c91c1ec218c559f2204cf08a6eff3106f9
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-13 12:34:34 -0300

    feat: block to see block number and time

--- contracts/GlobalVariables.sol
+++ contracts/GlobalVariables.sol
@@ -3,6 +3,11 @@
 pragma solidity ^0.8.7;
 
 contract GlobalVariables {
-    address public sender = msg.sender; // endereço público de quem está enviando a transação
+    address public sender = msg.sender;
     uint public value = msg.value;
+
+    uint public timestamp = block.timestamp;
+    uint public blockNuember = block.number;
+
+    address public oring = tx.origin
 }
