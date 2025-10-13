1cb3682a7ccb544b4edb5497f45e165058db7ba6
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-13 13:02:08 -0300

    feat: how to use Intance

--- contracts/Function.sol
+++ contracts/Function.sol
@@ -4,17 +4,34 @@ pragma solidity ^0.8.7;
 
 contract HelperFunctions {
     function soma(uint256 x, uint256 y) public returns(uint256) {
-        sub(2, 1);
+        //sub(2, 1);
         return x + y;
     }
     function sub(uint256 x, uint256 y) external returns(uint256) {
         soma(2,3);
+        fnInternal();
         return x + y;
     }
     function fnInternal() internal {
+        fnPrivate();
         uint256 x = 100;
     }
     function fnPrivate() private  {
         uint256 x = 200;
     }
+}
+
+contract ContractA {
+    HelperFunctions helperIntance = new HelperFunctions();
+
+    function foo() public {
+        helperIntance.soma(2,3); // public
+        helperIntance.sub(2, 1); // external
+        //helperIntance.fnInternal(); // Internal, não pode acessar
+        //helperIntance.private(); // Private, não pode acessar
+    }
+}
+
+contract ContractB is HelperFunctions {
+    
 }
