fd707d1baeb12eb3ea1d8a8edc8cff816f2e32b8
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-16 11:49:00 -0300

    feat: Function returnNamed

--- contracts/FunctionsReturns.sol
+++ contracts/FunctionsReturns.sol
@@ -6,4 +6,7 @@ contract FunctionReturns {
     function returnMany() public pure returns(uint, bool, string memory) {
         return (2, true, "Hi");
     }
+    function returnNamed() public pure returns(uint a, bool b) {
+        return (2, true);
+    }
 }
