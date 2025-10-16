ee856d918272ab5cde83777630ac21650c4f7fef
Buenohy <gabriel.buenohyginoc@gmail.com>
2025-10-16 12:28:02 -0300

    feat: function doSomething using view and block timestamp

--- contracts/ViewAndPure.sol
+++ contracts/ViewAndPure.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.7;
 
 contract ViewAndPure {
     string myVar = "Minha String";
-    
+
     function getGreeting() external view returns(string memory) {
         return myVar;
+    }
+
+    function doSomething(uint256 a, uint256 b) external view returns(uint256) {
+        return a * (b + 42 ) + block.timestamp;
     }
 }
